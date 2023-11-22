9762399b5564fed94b6bec7f582663ede35745aa
cybershoe <adam@genericor.com>
2023-11-22 10:16:07 -0500

    Entity validator exception format

--- minihass/_validators.py
+++ minihass/_validators.py
@@ -4,12 +4,16 @@
 VALID_ENTITY_CATEGORIES = ["diagnostic", "config"]
 
 
-def validate_entity_category(category: str):
+def validate_entity_category(category: str): -> bool
     """Validates that the entity category is an allowed value
 
     :param category: Entity category to validate
     :type category: str
-    :raises ValueError: _description_
+    :raises ValueError: The entity category is not valid
     """
     if category not in VALID_ENTITY_CATEGORIES:
-        raise ValueError("Invalid entity category")
+        raise ValueError(
+            f"Invalid entity category \"{category}\", must be one of ({'|'.join(VALID_ENTITY_CATEGORIES)})"
+        )
+
+    return True
