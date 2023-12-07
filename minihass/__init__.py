eca778d67572287773a76d707023b44ee6d1595e
cybershoe <adam@genericor.com>
2023-12-07 16:14:43 -0500

    replace __all__ for sphinx

--- minihass/__init__.py
+++ minihass/__init__.py
@@ -4,3 +4,5 @@ intended for use with CircuitPython.
 from .binary_sensor import BinarySensor
 from .device import Device
 from .entity import Entity
+
+__all__ = ["BinarySensor", "Device", "Entity"]
