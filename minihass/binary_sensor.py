1da643bab0f298d67d89648ed8edf4c11a30d3bd
cybershoe <adam@genericor.com>
2023-12-14 16:53:23 -0500

    Remove pointless change

--- minihass/binary_sensor.py
+++ minihass/binary_sensor.py
@@ -32,7 +32,6 @@ class BinarySensor(Entity, SensorEntity):
         **kwargs
     ):
         self.expire_after = validators.validate_bool(expire_after)
-        foo = "bar"
         self.force_update = validators.validate_bool(force_update)
 
         self.component_config = {
