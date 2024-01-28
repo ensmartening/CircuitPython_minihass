300d1a128203270a4d492d0a5c05ef91cdf1098e
Adam Schumacher <adam@genericor.com>
2024-01-28 17:22:03 -0500

    Constantized device config fields

--- minihass/const.py
+++ minihass/const.py
@@ -13,3 +13,23 @@ class QueueMode(Enum):
 QueueMode.NO = QueueMode()
 QueueMode.YES = QueueMode()
 QueueMode.ALWAYS = QueueMode()
+
+CONFIG_AVAILABILITY = "avty"
+CONFIG_CONNECTIONS = "cns"
+CONFIG_DEVICE = "dev"
+CONFIG_ENABLED_BY_DEFAULT = "en"
+CONFIG_ENCODING = "e"
+CONFIG_FORCE_UPDATE = "force_update"
+CONFIG_HARDWARE = "hw"
+CONFIG_IDENTIFIERS = "ids"
+CONFIG_NAME = "name"
+CONFIG_DEVICE_CLASS = "dev_cla"
+CONFIG_ENTITY_CATEGORY = "ent_cat"
+CONFIG_EXPIRE_AFTER = "expire_after"
+CONFIG_ICON = "ic"
+CONFIG_MANUFACTURER = "mf"
+CONFIG_PAYLOAD_OFF = "pl_off"
+CONFIG_PAYLOAD_ON = "pl_on"
+CONFIG_STATE_TOPIC = "stat_t"
+CONFIG_TOPIC = "t"
+CONFIG_UNIQUE_ID = "unique_id"
